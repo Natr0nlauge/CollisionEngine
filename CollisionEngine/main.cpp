03fdff3f47cc8160cf46cc3b36316b8ce2944be7
Sebastian Strobach <sebastian@dr-strobach.de>
2025-01-23 01:14:33 +0100

    fixed main.cpp file

--- CollisionEngine/main.cpp
+++ CollisionEngine/main.cpp
@@ -1,52 +1,84 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
 #include "Simulation.hpp"
 
+// TODO: fix all the warnings
+
+// TODO: implement proper tests
+
+// TODO: use doxygen
+
+// TODO: Replace simulation border with a seperate class
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
+    // TODO why is the indent weird?
+    // Example program
     std::vector<RigidBody *> collisionPartners;
-
-	// std::vector<sf::Vector2f> exampleVertices = { sf::Vector2f(25.0f, -50.0f), sf::Vector2f(-25.0f, -50.0f),sf::Vector2f(-50.0f, 0.0f),
+    std::vector<BoundaryElement *> boundaryElements;
+    Simulation & simRef = Simulation::getInstance();
+    // std::vector<sf::Vector2f> exampleVertices = { sf::Vector2f(25.0f, -50.0f),
+    // sf::Vector2f(-25.0f, -50.0f),sf::Vector2f(-50.0f, 0.0f),
     // sf::Vector2f(-50.0f, 25.0f), sf::Vector2f(25.0f, 25.0f)     };
-    std::vector<sf::Vector2f> exampleVertices = {sf::Vector2f(25.0f, -25.0f), sf::Vector2f(-25.0f, -25.0f), sf::Vector2f(-25.0f, 25.0f),
-            sf::Vector2f(25.0f, 25.0f)};
+    std::vector<sf::Vector2f> exampleVertices = {sf::Vector2f(25.0f, -25.0f), sf::Vector2f(-25.0f, -50.0f), sf::Vector2f(-25.0f, 25.0f),
+            sf::Vector2f(-20.0f, 30.0f), sf::Vector2f(25.0f, 25.0f)};
+    // collisionPartners.push_back(new Polygon(0.1, exampleVertices));
+    Polygon * playerPtr = new Polygon(0.1, exampleVertices);
+    simRef.addPlayer(playerPtr);
+    // std::vector<sf::Vector2f> exampleVertices2 = { sf::Vector2f(25.0f, -25.0f),
+    //// sf::Vector2f(-25.0f, -25.0f), sf::Vector2f(-50.0f, 0.0f),
+    //// sf::Vector2f(-75.0f, 225.0f), sf::Vector2f(25.0f, 25.0f)     };
+    std::vector<sf::Vector2f> exampleVertices2 = {sf::Vector2f(100.0f, -100.0f), sf::Vector2f(-100.0f, -100.0f),
+            sf::Vector2f(-100.0f, 100.0f), sf::Vector2f(100.0f, 100.0f)};
+    /* std::vector<sf::Vector2f> exampleVertices3 = {
+        {21.6506271f, -12.5000153f},      {12.4999876f, -21.6506424f},
+        {-1.16228075e-05f, -25.0000000f}, {-12.5000076f, -21.6506310f},
+        {-21.650636f, -12.4999952f},      {-25.0000000f, 3.77489505e-06f},
+        {-21.6506348f, 12.5000109f},      {-12.500019f, 21.6506348f},
+        {1.0927847e-06f, 25.0000000f},    {12.499990f, 21.6506367f},
+        {21.6506348f, 12.5000000f},       {25.0f, 0.0f}};*/
+    // collisionPartners.push_back(new Polygon(0.1, exampleVertices2));
+
+    // Test bodies
     collisionPartners.push_back(new Polygon(0.1, exampleVertices));
-    
-    // std::vector<sf::Vector2f> exampleVertices2 = { sf::Vector2f(25.0f, -25.0f), sf::Vector2f(-25.0f, -25.0f), sf::Vector2f(-50.0f, 0.0f),
-    // sf::Vector2f(-75.0f, 225.0f), sf::Vector2f(25.0f, 25.0f)     };
-    std::vector<sf::Vector2f> exampleVertices2 = {sf::Vector2f(25.0f, -25.0f), sf::Vector2f(-25.0f, -25.0f), sf::Vector2f(-25.0f, 25.0f),
-            sf::Vector2f(25.0f, 25.0f)};
-    collisionPartners.push_back(new Circle(0.1, 25));
+    collisionPartners[0]->setPosition({100.0f, 400.0f});
+    collisionPartners[0]->setVelocity(sf::Vector2f(0, -50));
+    collisionPartners[0]->setAngularVelocity(-50);
 
-    // Simulation border TODO: Replace with a seperate class
-    std::vector<sf::Vector2f> borderVertices3 = {sf::Vector2f(0.0f, 50.0f), sf::Vector2f(450.0f, 50.0f), sf::Vector2f(450.0f, 0.0f),
-            sf::Vector2f(0.0f, 0.0f)};
-    collisionPartners.push_back(new Polygon(0.0, borderVertices3));
-    std::vector<sf::Vector2f> borderVertices4 = {sf::Vector2f(0.0f, 50.0f), sf::Vector2f(450.0f, 50.0f), sf::Vector2f(450.0f, 0.0f),
-            sf::Vector2f(0.0f, 0.0f)};
-    collisionPartners.push_back(new Polygon(0.0, borderVertices4));
-    std::vector<sf::Vector2f> borderVertices5 = {sf::Vector2f(0.0f, 450.0f), sf::Vector2f(50.0f, 450.0f), sf::Vector2f(50.0f, 0.0f),
-            sf::Vector2f(0.0f, 0.0f)};
-    collisionPartners.push_back(new Polygon(0.0, borderVertices5));
-    std::vector<sf::Vector2f> borderVertices6 = {sf::Vector2f(0.0f, 450.0f), sf::Vector2f(50.0f, 450.0f), sf::Vector2f(50.0f, 0.0f),
-            sf::Vector2f(0.0f, 0.0f)};
-    collisionPartners.push_back(new Polygon(0.0, borderVertices6));
+    collisionPartners.push_back(new Circle(0.1));
+    collisionPartners[1]->setPosition({100.0f, 300.0f});
 
-    collisionPartners[0]->setPosition(200.0f, 160.0f);
-    collisionPartners[0]->setVelocity(sf::Vector2f(50.0f, 0.0f));
-    collisionPartners[0]->setAngularVelocity(0.0f);
-    collisionPartners[0]->setRotation(0.0f);
-    collisionPartners[1]->setPosition(400.0f, 200.0f);
-    collisionPartners[1]->setVelocity(sf::Vector2f(-50.0f, 0.0f));
-    collisionPartners[1]->setAngularVelocity(0.0f);
+    playerPtr->setRotation(180.0f);
+    playerPtr->setPosition(340.0f, 200.0f);
+    playerPtr->setVelocity(sf::Vector2f(0, 0.0f));
+    // collisionPartners[0]->setVelocity(sf::Vector2f(0.0f, -40.0f));
+    playerPtr->setAngularVelocity(0);
 
-    collisionPartners[2]->setPosition(256.0f, 5.0f);
-    collisionPartners[3]->setPosition(256.0f, 507.0f);
-    collisionPartners[4]->setPosition(5.0f, 256.0f);
-    collisionPartners[5]->setPosition(507.0f, 256.0f);
+    boundaryElements.push_back(new BoundaryElement(510));
+    boundaryElements[0]->setPosition(256, 0);
+    boundaryElements[0]->setRotation(180);
+    boundaryElements.push_back(new BoundaryElement(510));
+    boundaryElements[1]->setPosition(256, 512);
+    boundaryElements[1]->setRotation(0);
+    boundaryElements.push_back(new BoundaryElement(510));
+    boundaryElements[2]->setPosition(0, 256);
+    boundaryElements[2]->setRotation(90);
+    boundaryElements.push_back(new BoundaryElement(510));
+    boundaryElements[3]->setPosition(512, 256);
+    boundaryElements[3]->setRotation(270);
 
-	Simulation & simRef = Simulation::getInstance();
-    simRef.initBodies(collisionPartners);
+    // borders
+    // int n = collisionPartners.size();
+    /*collisionPartners[n - 4]->setPosition(256.0f, 5.0f);
+    collisionPartners[n - 3]->setPosition(256.0f, 507.0f);
+    collisionPartners[n - 2]->setPosition(5.0f, 256.0f);
+    collisionPartners[n - 1]->setPosition(507.0f, 256.0f);*/
+
     simRef.initWindow();
-	simRef.run();
-	
+    simRef.initBodies(collisionPartners);
+    simRef.initBoundaries(boundaryElements);
+    simRef.run();
+
+    return EXIT_SUCCESS;
 }
